2b469fbc0da7b0aa9ed84442daa6f6353358dba6
Jhon Quitian <jhon.quitian@udea.edu.co>
2022-03-24 11:39:38 -0500

    Primer commit

---
+++ app.js
@@ -0,0 +1,59 @@
+require('colors');
+
+const { guardarDB, leerDB } = require('./helpers/guardarArchivo');
+const { inquirerMenu, pausa, leerInput, listadoTareasBorrar, confirmar } = require('./helpers/inquirer');
+const Tarea = require('./models/tarea');
+const Tareas = require('./models/tareas');
+
+const main = async() => {
+
+    let opt = '';
+    const tareas = new Tareas();
+
+    const tareasDB = leerDB();
+    if(tareasDB){
+        tareas.cargarTareasFromArray(tareasDB);
+    }
+
+    // await pausa();
+
+    do {
+        // Imprimir el menú
+        opt = await inquirerMenu();
+
+        switch (opt) {
+            case '1':
+                const desc = await leerInput('Descripción:');
+                tareas.crearTarea(desc);
+                break;
+            case '2':
+                tareas.listadoCompleto();
+                // console.log(tareas.listadoArr);
+                break;
+            case '3': // listar completadas
+                tareas.listarPendienteCompletadas(true);
+                break;
+            case '4': // Listar pendientes
+                tareas.listarPendienteCompletadas(false);
+                break;
+            case '6':
+
+                const id = await listadoTareasBorrar(tareas.listadoArr);
+                const ok = await confirmar('¿Está seguro?');
+                if(ok) tareas.borrarTarea(id)
+                break;
+        }
+
+        guardarDB(tareas.listadoArr);
+
+        await pausa();
+        
+        
+    } while (opt !== '0');
+    
+    
+    
+    // pausa();
+}
+
+main();
